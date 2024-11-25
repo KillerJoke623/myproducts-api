fd05289b00b554361e8483f26aa0ad489faa6c93
Глеб Заплатин <gleb.zaplatin@outlook.com>
2024-11-25 16:26:17 +0300

    Initial commit

---
+++ src/main/java/com/romashkaco/myproducts_api/MyproductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.romashkaco.myproducts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyproductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyproductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romashkaco/myproducts_api/MyproductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romashkaco.myproducts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyproductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
